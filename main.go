f35484af41dbdcee217ab0ba188371df718ea13b
lintao <mrqter@gmail.com>
2018-01-24 16:39:01 +0800

    update

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"amazonApi/models"
 	"fmt"
 	"net/http"
-	"os"
 	"strconv"
 	"time"
 
@@ -18,8 +17,8 @@ import (
 
 func main() {
 	e := echo.New()
-	orm.Debug = true
-	orm.DebugLog = orm.NewLog(os.Stdout)
+	//orm.Debug = true
+	//orm.DebugLog = orm.NewLog(os.Stdout)
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
