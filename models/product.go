4b216848adfed6ae52e6e84bf76a4a562fcb884d
lintao <mrqter@gmail.com>
2018-01-26 11:51:08 +0800

    update

--- models/product.go
+++ models/product.go
@@ -3,14 +3,14 @@ package models
 import "github.com/astaxie/beego/orm"
 
 type Product struct {
-	UserProfile string `orm:"-" json:"user_profile"`
-	Id          int64
-	Url         string
-	CategoryId  int64
+	UserProfile string   `orm:"-" json:"user_profile"`
+	Id          int64    `json:"id"`
+	Url         string   `json:"url"`
+	CategoryId  int64    `json:"category_id"`
 	Categorys   []string `orm:"-" json:"categorys"`
-	Name        string
-	UserId      int64
-	ReviewUrl   string
+	Name        string   `json:"name"`
+	UserId      int64    `json:"user_id"`
+	ReviewUrl   string   `json:"review_url"`
 }
 
 func init() {
